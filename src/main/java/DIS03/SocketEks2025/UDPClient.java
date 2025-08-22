0e44e1b911355f7ea0d36c126f6b82678a83243c
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2025-08-22 17:34:31 +0200

    opgave lektion 3

---
+++ src/main/java/DIS03/SocketEks2025/UDPClient.java
@@ -0,0 +1,29 @@
+package DIS03.SocketEks2025;
+
+import java.io.*;
+import java.net.*;
+
+class UDPClient {
+	public static void main(String args[]) throws Exception {
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(
+				System.in));
+		DatagramSocket clientSocket = new DatagramSocket();
+		clientSocket.setBroadcast(true);
+		InetAddress IPAddress = InetAddress.getByName("255.255.255.255");
+		byte[] sendData = new byte[1024];
+		byte[] receiveData = new byte[1024];
+
+		String sentence = inFromUser.readLine();
+		sendData = sentence.getBytes();
+		DatagramPacket sendPacket = new DatagramPacket(sendData,
+				sendData.length, IPAddress, 9995);
+		clientSocket.send(sendPacket);
+		DatagramPacket receivePacket = new DatagramPacket(receiveData,
+				receiveData.length);
+		clientSocket.receive(receivePacket);
+
+		String modifiedSentence = new String(receivePacket.getData());
+		System.out.println("FROM SERVER:" + modifiedSentence);
+		clientSocket.close();
+	}
+}
