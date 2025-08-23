f696a384543b8748c333f59b2c499c7faf015966
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2025-08-23 11:50:15 +0200

    Made a TCP Client and server. The server holds the connection open when the client exists. So new clients can join. Only one client at a time

---
+++ src/main/java/TCP_Template/TcpClient.java
@@ -0,0 +1,47 @@
+package TCP_Template;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class TcpClient {
+    public static void main(String[] args) {
+
+        Socket socket = null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+
+        try{
+            socket = new Socket("localhost", 1234);
+
+            inputStreamReader = new InputStreamReader(socket.getInputStream());
+            outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+
+            bufferedReader = new BufferedReader(inputStreamReader);
+            bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+            Scanner scanner = new Scanner(System.in);
+
+            while (true){
+                String msgToSend = scanner.nextLine();
+                bufferedWriter.write(msgToSend);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+
+                System.out.println("Server: " + bufferedReader.readLine());
+
+                if (msgToSend.equalsIgnoreCase("BYE")){
+                    break;
+                }
+            }
+
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/TCP_Template/TcpServer.java
@@ -0,0 +1,54 @@
+package TCP_Template;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TcpServer {
+    public static void main(String[] args) {
+        Socket socket = null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+
+        ServerSocket serverSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(1234); //ServerSocket gør at vi lyter på 1234.
+
+            while (true){
+                socket = serverSocket.accept(); //serSocker.accpept betyder at vi venter på at nogle kommer ind på porten
+                inputStreamReader = new InputStreamReader(socket.getInputStream());
+                outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+
+                bufferedReader = new BufferedReader(inputStreamReader);
+                bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+                while (true){
+                    String msgFromClient = bufferedReader.readLine();
+
+                    System.out.println("Client: " + msgFromClient);
+
+                    bufferedWriter.write("MSG Received.");
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+
+                    if (msgFromClient.equalsIgnoreCase("BYE")){
+                        break;
+                    }
+                }
+
+                socket.close();
+                inputStreamReader.close();
+                outputStreamWriter.close();
+                bufferedReader.close();
+                bufferedWriter.close();
+            }
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
